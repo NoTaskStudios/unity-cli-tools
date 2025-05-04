36d1c0011b663c88e321199ab953edc412b10ada
Matheus Costa <matheus.design12@gmail.com>
2025-05-04 16:32:57 -0300

    fix(hot-fix): re-import event modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as UnityHub } from "./unityHub.ts";
 export { default as UnityEditor } from "./unityEditor.ts";
+export { UnityHubInstallerEvent } from "./events/hubEventEmitter.ts";
 
 export * from "./types/unity.ts";
